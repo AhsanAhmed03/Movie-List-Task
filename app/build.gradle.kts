6a644973ec8ec502e02360314b876c03244e1086
Ahsan Ahmed <aahsanaahmed26@gmail.com>
2025-11-21 23:03:46 +0500

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.app.movies.list"
-        minSdk = 28
+        minSdk = 27
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
